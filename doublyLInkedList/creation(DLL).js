661fb2781b80eba2760bd93f4124d948de3277a2
Arafath <arafathctk@gmail.com>
2024-02-03 07:48:44 +0530

    doubly linked list

---
+++ doublyLInkedList/creation(DLL).js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  prepend(data) {
+    let newNode = new Node(data);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (this.tail === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+}
